7326f7a2f583dfc46b45ebf480be1fedce96e6f3
Paulo Cesar <pc_pinhal@yahoo.com.br>
2023-12-06 20:27:50 -0300

    Banco de dados Corrigido

--- banco.sql
+++ banco.sql
@@ -6,6 +6,7 @@ id_login        INT NOT NULL AUTO_INCREMENT,
 login           VARCHAR(255) NOT NULL,
 senha           VARCHAR(255) NOT NULL,
 nome            VARCHAR(255),
+foto				 VARCHAR(255),
 PRIMARY KEY(id_login)
 );
 CREATE TABLE tb_noticia(
@@ -13,14 +14,15 @@ id_noticia      INT NOT NULL AUTO_INCREMENT,
 noticia         TEXT NOT NULL,
 foto            VARCHAR(255),
 fk_de           INT NOT NULL,
+datahora			 DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 PRIMARY KEY (id_noticia),
 FOREIGN KEY (fk_de) REFERENCES tb_login (id_login)
 );
 
-INSERT INTO tb_login (login, senha, nome) VALUES
-("admin","123","Administrador"),
-("local","123","Conta Local"),
-("guess","123","Conta Convidado");
+INSERT INTO tb_login (login, senha, nome, foto) VALUES
+("admin","123","Administrador","login1.jpg"),
+("local","123","Conta Local","login2.jpg"),
+("guess","123","Conta Convidado","");
 
 INSERT INTO tb_noticia (noticia, foto, fk_de) VALUES
 ("Teste do site","foto1.jpg",1),
--- index.php
+++ index.php
@@ -1,3 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Fakebook - seu site de noticias</title>
+</head>
+<body>
+    <div class="container">
+
+    </div>
+</body>
+</html>
+
 <?php
-phpinfo();
+
 ?>
