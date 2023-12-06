37e51f903a8f190af4cc0828d116b1cc0ee4a24d
Paulo Cesar <pc_pinhal@yahoo.com.br>
2023-12-06 19:37:33 -0300

    Banco de dados Criado

---
+++ banco.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE IF NOT EXISTS Fakebook;
+USE Fakebook;
+-- DROP DATABASE Fakebook;
+CREATE TABLE tb_login(
+id_login        INT NOT NULL AUTO_INCREMENT,
+login           VARCHAR(255) NOT NULL,
+senha           VARCHAR(255) NOT NULL,
+nome            VARCHAR(255),
+PRIMARY KEY(id_login)
+);
+CREATE TABLE tb_noticia(
+id_noticia      INT NOT NULL AUTO_INCREMENT,
+noticia         TEXT NOT NULL,
+foto            VARCHAR(255),
+fk_de           INT NOT NULL,
+PRIMARY KEY (id_noticia),
+FOREIGN KEY (fk_de) REFERENCES tb_login (id_login)
+);
+
+INSERT INTO tb_login (login, senha, nome) VALUES
+("admin","123","Administrador"),
+("local","123","Conta Local"),
+("guess","123","Conta Convidado");
+
+INSERT INTO tb_noticia (noticia, foto, fk_de) VALUES
+("Teste do site","foto1.jpg",1),
+("Noticia extraordinaria","foto2.jpg",2),
+("Cerveja gratis","",3);
+
+SELECT * FROM tb_login;
+SELECT * FROM tb_noticia;
